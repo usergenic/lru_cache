26cb5e1ca8a0eb6d622551b65f484baaef9eb33b
Brendan Baldwin <brendan@usergenic.com>
2012-11-09 22:29:02 -0800

    Added errors, new methods and better specs to the LRUCache.

--- lib/lru_cache.rb
+++ lib/lru_cache.rb
@@ -1,15 +1,38 @@
 class LRUCache
 
+  Error             = Class.new RuntimeError
+  InvalidLimitError = Class.new Error
+  NoLimitError      = Class.new Error
+
   Node = Struct.new(:key, :value, :mru, :lru)
 
   attr_reader :count, :limit
 
-  def initialize
+  # Sets up the new cache, optionally assigning given limit.
+  def initialize(limit=nil)
     @index = {}
     @count = 0
-    @limit = 1
+    self.limit = limit if limit
   end
 
+  # Empties the cache of all values.
+  def clear
+    count.times.each { delete_lru }
+  end
+
+  # Remove and return value from cache.
+  def delete(key)
+    node = @index.delete(key)
+    return unless node
+    @count -= 1
+    node.mru.lru = node.lru if node.mru
+    node.lru.mru = node.mru if node.lru
+    @mru = node.lru if @mru == node
+    @lru = node.mru if @lru == node
+    node.value
+  end
+
+  # Return value from cache and sets it as most-recently-used.
   def get(key)
     node = @index[key]
     if node
@@ -18,30 +41,35 @@ class LRUCache
     end
   end
 
-  alias [] get
-
+  # Assigns a new limit to the cache.  Deletes least recently used
+  # values if new limit is less than the count.
   def limit=(new_limit)
-    (count - new_limit).times { drop_lru }
+    new_limit and new_limit >= 1 or raise InvalidLimitError, "limit must be 1 or greater"
+    extra_count = count - new_limit
+    extra_count.times { delete_lru }
     @limit = new_limit
   end
 
+  # Stores a value in the cache.
   def set(key, value)
     node = @index[key]
     unless node
       node = Node.new(key)
       @index[key] = node
       @count += 1
-      drop_lru if count > limit
+      delete_lru if count > ensure_limit
     end
     set_mru(node)
     node.value = value
   end
 
+  alias [] get
   alias []= set
 
   private
 
-  def drop_lru
+  # Deletes the least recently used value from the cache.
+  def delete_lru
     return unless @lru
     @count -= 1
     @index.delete(@lru.key)
@@ -49,6 +77,12 @@ class LRUCache
     @lru.lru = nil if @lru
   end
 
+  # Returns the limit if set, otherwise raises exception.
+  def ensure_limit
+    limit || raise(NoLimitError, "limit not set for LRUCache")
+  end
+
+  # Advances a node to the most recently used spot.
   def set_mru(node)
     return if node == @mru
     @mru.mru = node if @mru
--- spec/lru_cache_spec.rb
+++ spec/lru_cache_spec.rb
@@ -2,42 +2,31 @@ require "lru_cache"
 
 describe LRUCache do
 
-  let :cache do
-    LRUCache.new
-  end
+  alias cache subject
 
-  describe "#[]" do
+  describe ".new" do
 
-    it "is an alias for get" do
-      cache.method(:[]).should == cache.method(:get)
+    it "allows assignment of the limit" do
+      LRUCache.new.limit.should be_nil
+      LRUCache.new(5).limit.should == 5
     end
   end
 
-  describe "#[]=" do
+  describe "#clear" do
 
-    it "is an alias for set" do
-      cache.method(:[]=).should == cache.method(:set)
-    end
-  end
-
-  describe "#get" do
-
-    it "returns an item for key when present in the cache" do
+    it "empties the cache of all items" do
+      cache.limit = 3
+      cache.count.should == 0
       cache.set("x", :x)
+      cache.set("y", :y)
+      cache.set("z", :z)
+      cache.count.should == 3
       cache.get("x").should == :x
-    end
 
-    it "returns nil when key is not in the cache" do
+      cache.clear
+      
+      cache.count.should == 0
       cache.get("x").should be_nil
-    end
-  end
-
-  describe "#set" do
-
-    it "puts an item in the cache" do
-      cache.get("x").should be_nil
-      cache.set("x", :x)
-      cache.get("x").should == :x
     end
   end
 
@@ -55,6 +44,33 @@ describe LRUCache do
     end
   end
 
+  describe "#delete" do
+
+    it "removes the item from the cache" do
+      cache.limit = 1
+      cache.count.should == 0
+      cache.set("x", :x)
+      cache.get("x").should == :x
+      cache.count.should == 1
+      cache.delete("x").should == :x
+      cache.count.should == 0
+      cache.get("x").should be_nil
+    end
+  end
+
+  describe "#get" do
+
+    it "returns an item for key when present in the cache" do
+      cache.limit = 1
+      cache.set("x", :x)
+      cache.get("x").should == :x
+    end
+
+    it "returns nil when key is not in the cache" do
+      cache.get("x").should be_nil
+    end
+  end
+
   describe "#limit" do
 
     it "returns the maximum number of items in cache" do
@@ -62,12 +78,16 @@ describe LRUCache do
       cache.limit.should == 500
     end
 
-    it "defaults to 1" do
-      cache.limit.should == 1
+    it "has no default value" do
+      LRUCache.new.limit.should be_nil
     end
   end
 
   describe "#limit=" do
+
+    it "does not accept numbers less than 1" do
+      expect { cache.limit = 0 }.to raise_error LRUCache::InvalidLimitError
+    end
 
     it "sets the maximum number of items in cache" do
       cache.limit = 500
@@ -75,7 +95,6 @@ describe LRUCache do
     end
 
     it "drops items from cache if limit is newly less than count" do
-
       cache.limit = 3
 
       cache.set("x", :x)
@@ -90,6 +109,34 @@ describe LRUCache do
       cache.get("x").should be_nil
       cache.get("y").should == :y
       cache.get("z").should == :z
+    end
+  end
+
+  describe "#set" do
+
+    it "puts an item in the cache" do
+      cache.limit = 1
+      cache.get("x").should be_nil
+      cache.set("x", :x)
+      cache.get("x").should == :x
+    end
+
+    it "will raise an exception when limit is not set" do
+      expect { LRUCache.new.set("x", :x) }.to raise_error LRUCache::NoLimitError
+    end
+  end
+
+  describe "#[]" do
+
+    it "is an alias for get" do
+      cache.method(:[]).should == cache.method(:get)
+    end
+  end
+
+  describe "#[]=" do
+
+    it "is an alias for set" do
+      cache.method(:[]=).should == cache.method(:set)
     end
   end
 end
